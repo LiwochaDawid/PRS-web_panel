08a9e075189a6b1bf01ab003dc57b4fd13d743e2
Dawid <liwocha.dawid@gmail.com>
2017-12-04 15:55:03 +0100

    login and javascripts (request not working yet)

---
+++ scripts/request.js
@@ -0,0 +1,23 @@
+﻿var secret = "Basic cHJzOnNlY3JldA==";
+var mainPath = "http://localhost:8080/";
+
+function loginRequest(username, password) {
+	var path = mainPath+"token";
+	var request = new XMLHttpRequest();
+    request.onreadystatechange = state_change;
+	request.open("POST", path, false);
+	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	request.setRequestHeader("Authorization", secret);
+	request.send("grant_type=password&username="+username+"&password="+password);
+    function state_change() {
+		alert(request.readystate)
+		if (request.readyState == 4) {
+			if (request.status == 200) {
+				var response = JSON.parse(request.responseText);
+				token = response.access_token;
+				return token;
+			}
+		}
+	}
+	return request.status;
+}
